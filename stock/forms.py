09ecf5fed6ff9e5826df080d550d887f112d718c
pfbrodon <pfbrodon@gmail.com>
2024-01-21 18:25:57 -0300

    ok forms

--- stock/forms.py
+++ stock/forms.py
@@ -6,23 +6,32 @@ class NuevoForm(ModelForm):
         model= Producto
         fields=('codigo', 'categoria' , 'marca' , 'descripcion' , 'cantidad' , 'utilidad' , 'precioCosto' )
         widgets ={
-            'codigo': forms.NumberInput(attrs={'class': 'form-control','id':'codigo'}),
-            'categoria': forms.Select(attrs={'class': 'form-select'}),
-            'marca': forms.Select(attrs={'class': 'form-select','id':'marca'}),
-            'descripcion': forms.TextInput(attrs={'class': 'form-control', 'id':'descripcion'}),
-            'cantidad': forms.NumberInput(attrs={'class': 'form-control','id':'cantidad'}),
-            'utilidad': forms.Select(attrs={'class': 'form-select'}),
-            'precioCosto': forms.NumberInput(attrs={'class': 'form-control'}),
+            'codigo': forms.NumberInput(attrs={'class': 'form-control','id':'codigo','style': 'width: 150px;'}),
+            'categoria': forms.Select(attrs={'class': 'form-select','style': 'width: 150px;'}),
+            'marca': forms.Select(attrs={'class': 'form-select','id':'marca','style': 'width: 150px;'}),
+            'descripcion': forms.TextInput(attrs={'class': 'form-control', 'id':'descripcion','style': 'width: 500px;'}),
+            'cantidad': forms.NumberInput(attrs={'class': 'form-control','id':'cantidad','style': 'width: 150px;'}),
+            'utilidad': forms.Select(attrs={'class': 'form-select','style': 'width: 150px;'}),
+            'precioCosto': forms.NumberInput(attrs={'class': 'form-control','style': 'width: 150px;'}),
             
         }
 class ProductoForm(ModelForm):
     class Meta:
         model= Producto
-        fields=('codigo', 'categoria' , 'marca' , 'descripcion' , 'cantidad' , 'utilidad' )
+        fields=('codigo', 'categoria' , 'marca' , 'descripcion' , 'cantidad' , 'utilidad' ,'precioCosto')
+        widgets ={
+            'codigo': forms.NumberInput(attrs={'class': 'form-control','id':'codigo','style': 'width: 150px;'}),
+            'categoria': forms.Select(attrs={'class': 'form-select','style': 'width: 150px;'}),
+            'marca': forms.Select(attrs={'class': 'form-select','id':'marca','style': 'width: 150px;'}),
+            'descripcion': forms.TextInput(attrs={'class': 'form-control', 'id':'descripcion','style': 'width: 500px;'}),
+            'cantidad': forms.NumberInput(attrs={'class': 'form-control','id':'cantidad','style': 'width: 150px;'}),
+            'utilidad': forms.Select(attrs={'class': 'form-select','style': 'width: 150px;'}),     
+            'precioCosto': forms.NumberInput(attrs={'class': 'form-select','style': 'width: 150px;'}),            
+        }
             
 class ProductoBusqueda(forms.Form):
     descripcion = forms.CharField(
         max_length=50,
         label= 'BUSQUEDA:',
-        widget=forms.TextInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={'class': 'form-control','style': 'width: 400px;'})
     )
